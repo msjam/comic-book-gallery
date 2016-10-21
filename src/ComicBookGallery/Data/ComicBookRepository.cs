e099a49750e3e739c70af7721171bed584490167
Michael Savransky <msjam5011@gmail.com>
2016-10-21 13:26:39 -0700

    Updated comic book list view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -59,6 +59,12 @@ namespace ComicBookGallery.Data
                     Favorite = false
                 }
             };
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int Id)
         {
             ComicBook comicBookToReturn = null;
